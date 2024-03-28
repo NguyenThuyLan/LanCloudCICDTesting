4ad7048797321b9dc05cba3b8906cc16ee3ca481
Lan Nguyen Thuy <lnt@umbraco.dk>
2024-03-28 16:10:39 +0700

    add content security header

--- src/UmbracoProject/Program.cs
+++ src/UmbracoProject/Program.cs
@@ -1,3 +1,5 @@
+using Joonasw.AspNetCore.SecurityHeaders;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.CreateUmbracoBuilder()
@@ -26,6 +28,52 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    await next();
+});
+
+app.UseCsp(csp =>
+{
+    csp.ByDefaultAllow
+        .FromSelf()
+        .From("packages.umbraco.org")
+        .From("our.umbraco.org");
+    csp.AllowScripts
+        .FromSelf()
+        .From("ajax.googleapis.com")
+        .From("unpkg.com")
+        .From("ajax.aspnetcdn.com")
+        .From("cdnjs.cloudflare.com")
+        .From("cdn.jsdelivr.net");
+    csp.AllowStyles
+        .FromSelf()
+        .AllowUnsafeInline()
+        .From("fonts.googleapis.com")
+        .From("cdn.jsdelivr.net")
+        .From("cdnjs.cloudflare.com")
+        .From("cdn.linearicons.com");
+    csp.AllowImages
+        .FromSelf()
+        .From("*.googleapis.com")
+        .From("via.placeholder.com")
+        .From("umbraco.com");
+    csp.AllowFonts
+        .FromSelf()
+        .From("cdnjs.cloudflare.com")
+        .From("fonts.gstatic.com")
+        .From("cdn.linearicons.com");
+
+    csp.AllowFraming.FromSelf();
+    csp.OnSendingHeader = context =>
+    {
+        context.ShouldNotSend = context.HttpContext.Request.Path.StartsWithSegments("/umbraco");
+        return Task.CompletedTask;
+    };
+});
 
 app.UseUmbraco()
     .WithMiddleware(u =>
